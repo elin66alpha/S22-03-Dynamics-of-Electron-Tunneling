4fb5a9fde2d4192ad3f90cc6c4e8f14adcc9778d
elin 66 <liupp12138@hotmail.com>
2023-02-18 01:55:49 -0500

    keithley import updated

--- scripts/processing/keithley_import2.py
+++ scripts/processing/keithley_import2.py
@@ -19,9 +19,12 @@ def position_process(str):
     return new_str
 
 def find_min_max(table):
-    print(table)
-    vmin = int(min(table.col_values(4 ,start_rowx=1)))
-    vmax = int(max(table.col_values(4,start_rowx=1)))
+    avmin = int(min(table.col_values(4 ,start_rowx=1)))
+    avmax = int(max(table.col_values(4,start_rowx=1)))
+    bvmin = int(min(table.col_values(2 ,start_rowx=1)))
+    bvmax = int(max(table.col_values(2 ,start_rowx=1)))
+    vmin = min(avmin,bvmin)
+    vmax = max(avmin,bvmin)
     return vmin,vmax
 
 def keithley_time(folder_name):
@@ -43,59 +46,70 @@ def keithley_time(folder_name):
 
         return loc
 
-def check_blank(line):
-    if line[2] == 'R' or line[2] =='S' or line[2] == 'VS' or line[2] == 'F':
-        if line[3] != '' and line[4]!='' and line[6]!='' and line[7]!='' and line[8] !='':
+def check_blank_line(line):
+    if (line[2] == 'R' or line[2] =='S' or line[2] == 'VS' or line[2] == 'F'or line[2] == 'H'or line[2] == 'C'):
+        if (line[3] != '' and line[4]!='' and line[8] !=''):
             return True
+    if(line[0] != 'Date'):
+        print(f'ERROR: please check the lab note format, {line} is missing parameters')
     return False
 
+def check_blank_cell(str):
+    changed_str = ''
+    if(str == ''):
+        changed_str = '?'
+    else:
+        changed_str = str
+    return changed_str
+        
 
 def sort(line,date):
 
     folder_name = ''
-    position = ''
+    position= ''
+
     activity= ''
     time = ''
     rr = ''
     wafer = ''
-    if(check_blank(line)):
+    if(check_blank_line(line)):
         if line[0] == date or date == 'all':
-            print(line[0])
+            folder_name = line[2]
             if(line[2] == 'R'):
                 activity = 'reset'
-                folder_name = 'Reset#1'
             elif(line[2] == 'S'):
                 activity = 'set'
-                folder_name = 'Set#1'
             elif(line[2] == 'F'):
                 activity = 'form'
-                folder_name = 'Set#1'
-            elif(line[2] == 'VS'):
+            elif(line[2] == 'VS' or line[2] == 'H'or line[2] == 'C'):
                 activity = 'observe'
-                folder_name = 'VerifySet_1#1'
+
+            wafer = check_blank_cell(line[1])
+            if(line[1] == 'Old Wafer'):
+                wafer = '0'
+            elif(line[1] == 'New Wafer (Scratched)'):
+                wafer = '1'
+            else:
+                wafer = '?'
 
 
-            wafer = line[1]
-            # if(line[1] == 'Old Wafer'):
-            #     wafer = '0'
-            # elif(line[1] == 'New Wafer (Scratched)'):
-            #     wafer = '1'
-            # else:
-            #     wafer = '?'
-
-            position = f'(wafer{wafer},{position_process(line[3])},-1,-1,{position_process(line[4])})'
-            icc = line[6].replace(' ','')
-            rr = line[7]
+            if(line[5]):
+                position = f'(wafer{wafer},{position_process(line[3])},-1,-1,{position_process(line[4])})_(wafer{wafer},{position_process(line[3])},-1,-1,{position_process(line[5])})'
+            else:
+                position = f'(wafer{wafer},{position_process(line[3])},-1,-1,{position_process(line[4])})'
+            icc = check_blank_cell(line[6]).replace(' ','')
+            rr =  check_blank_cell(line[7])
             run_num = line[8].split(',')
             for num in run_num:
                 try:
                     raw = xlrd.open_workbook(
-                        f'{folder_name}/Site@1/Run{str(num)}/data@1[{str(num)}].xls')
-                    time = keithley_time(f'{folder_name}/Site@1/Run{str(num)}')
+                        f'{folder_name}/Run{str(num)}/data@1[{str(num)}].xls')
+                    time = keithley_time(f'{folder_name}/Run{str(num)}')
                     time = time.strftime(r'%y%m%d%H%M%S')
                     table = raw.sheet_by_index(0)
                     vmin, vmax= find_min_max(table)
                     file_name = f'{position}_{time}_{activity}_{vmin}_{vmax}_{rr}_{icc}'
+                    
                     with open(f'orgnized/{file_name}.csv', 'w', encoding='utf-8') as f:
                         write = csv.writer(f)
                         write.writerow(['---'])
@@ -104,10 +118,10 @@ def sort(line,date):
                         for row_num in range(table.nrows):
                             row_value = table.row_values(row_num)
                             write.writerow(row_value)
+                    print(f'MESSAGE: {file_name} is generated successfully. Ignore the warning.')
                 except:
-                    print( f'{folder_name}/Site@1/Run{str(num)}/data@1[{str(num)}].xls is not found')
-        # else:
-        #     print(date)   
+                    print( f'ERROR: {folder_name}/Run{str(num)}/data@1[{str(num)}].xls is not found')
+ 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
